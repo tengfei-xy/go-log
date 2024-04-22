310fac91d671253d800eef4caa5233129876da8f
浔阳陌客 <virtualtengfei@163.com>
2024-04-22 20:34:34 +0800

    修改日志格式

--- log.go
+++ log.go
@@ -16,60 +16,60 @@ func log(level string, formating string, args ...interface{}) {
 	if ok {
 		filename = filepath.Base(filename)
 	}
-	fmt.Printf("%s %s:%d %s %s\n", time.Now().Format("2006-01-02 15:04:05"), filename, line, level, fmt.Sprintf(formating, args...))
+	fmt.Printf("%s %s %s:%d %s\n", level, time.Now().Format("2006-01-02 15:04:05"), filename, line, fmt.Sprintf(formating, args...))
 
 }
 
 func Debugf(formating string, args ...interface{}) {
 	if LOGLEVEL <= 0 {
-		log("debug", formating, args...)
+		log("D", formating, args...)
 	}
 }
 func Debug(formating interface{}) {
 	if LOGLEVEL <= 0 {
-		log("debug", fmt.Sprintf("%v", formating))
+		log("D", fmt.Sprintf("%v", formating))
 	}
 }
 func Infof(formating string, args ...interface{}) {
 	if LOGLEVEL <= 1 {
-		log("info ", formating, args...)
+		log("I", formating, args...)
 	}
 }
 func Info(formating interface{}) {
 	if LOGLEVEL <= 1 {
-		log("info ", fmt.Sprintf("%v", formating))
+		log("I", fmt.Sprintf("%v", formating))
 	}
 }
 func Warnf(formating string, args ...interface{}) {
 	if LOGLEVEL <= 2 {
-		log("warn ", formating, args...)
+		log("W", formating, args...)
 	}
 }
 func Warn(formating interface{}) {
 	if LOGLEVEL <= 2 {
-		log("warn ", fmt.Sprintf("%v", formating))
+		log("W", fmt.Sprintf("%v", formating))
 	}
 }
 
 func Errorf(formating string, args ...interface{}) {
 	if LOGLEVEL <= 3 {
-		log("error", formating, args...)
+		log("E", formating, args...)
 	}
 }
 func Error(formating interface{}) {
 	if LOGLEVEL <= 3 {
-		log("error", fmt.Sprintf("%v", formating))
+		log("E", fmt.Sprintf("%v", formating))
 	}
 }
 func Fatalf(formating string, args ...interface{}) {
 	if LOGLEVEL <= 4 {
-		log("fatal", formating, args...)
+		log("F", formating, args...)
 		os.Exit(125)
 	}
 }
 func Fatal(formating interface{}) {
 	if LOGLEVEL <= 4 {
-		log("fatal", fmt.Sprintf("%v", formating))
+		log("F", fmt.Sprintf("%v", formating))
 		os.Exit(125)
 	}
 }
