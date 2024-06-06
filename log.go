26f692a4dd5e0af90240d680c274f7f76cf682ed
tengfe-xy <taotengfei@zhiweidata.com>
2024-06-06 15:24:29 +0800

    修改获取日志等级

--- log.go
+++ log.go
@@ -48,88 +48,111 @@ func SetLevelDebug2() {
 func SetLevelDebug3() {
 	level = 7
 }
+func GetLevel() (int, string) {
+	var str string = "Info"
+
+	switch level {
+	case 0:
+		str = "Fatal"
+	case 1:
+		str = "Error"
+	case 2:
+		str = "Warn"
+	case 3:
+		str = "Info"
+	case 4:
+		str = "Debug"
+	case 5:
+		str = "Debug1"
+	case 6:
+		str = "Debug2"
+	case 7:
+		str = "Debug3"
+	}
+	return level, str
+}
 func Fatalf(formating string, args ...interface{}) {
-	if level <= 0 {
+	if level >= 0 {
 		log("F", formating, args...)
 		os.Exit(125)
 	}
 }
 
 func Fatal(formating interface{}) {
-	if level <= 0 {
+	if level >= 0 {
 		log("F", fmt.Sprintf("%v", formating))
 		os.Exit(125)
 	}
 }
 
 func Errorf(formating string, args ...interface{}) {
-	if level <= 1 {
+	if level >= 1 {
 		log("E", formating, args...)
 	}
 }
 func Error(formating interface{}) {
-	if level <= 1 {
+	if level >= 1 {
 		log("E", fmt.Sprintf("%v", formating))
 	}
 }
 func Warnf(formating string, args ...interface{}) {
-	if level <= 2 {
+	if level >= 2 {
 		log("W", formating, args...)
 	}
 }
 func Warn(formating interface{}) {
-	if level <= 2 {
+	if level >= 2 {
 		log("W", fmt.Sprintf("%v", formating))
 	}
 }
 func Infof(formating string, args ...interface{}) {
-	if level <= 3 {
+	if level >= 3 {
 		log("I", formating, args...)
 	}
 }
 func Info(formating interface{}) {
-	if level <= 3 {
+	if level >= 3 {
 		log("I", fmt.Sprintf("%v", formating))
 	}
 }
 
 func Debugf(formating string, args ...interface{}) {
-	if level <= 4 {
+	if level >= 4 {
 		log("D", formating, args...)
 	}
 }
 func Debug(formating interface{}) {
-	if level <= 4 {
+	if level >= 4 {
 		log("D", fmt.Sprintf("%v", formating))
 	}
 }
 func Debug1f(formating string, args ...interface{}) {
-	if level <= 5 {
+	if level >= 5 {
 		log("D", formating, args...)
 	}
 }
 func Debug1(formating interface{}) {
-	if level <= 5 {
+	if level >= 5 {
 		log("D", fmt.Sprintf("%v", formating))
 	}
 }
 func Debug2f(formating string, args ...interface{}) {
-	if level <= 6 {
+	if level >= 6 {
 		log("D", formating, args...)
 	}
 }
 func Debug2(formating interface{}) {
-	if level <= 6 {
+	if level >= 6 {
 		log("D", fmt.Sprintf("%v", formating))
 	}
 }
 func Debug3f(formating string, args ...interface{}) {
-	if level <= 7 {
+	if level >= 7 {
 		log("D", formating, args...)
 	}
 }
 func Debug3(formating interface{}) {
-	if level <= 7 {
+	if level >= 7 {
 		log("D", fmt.Sprintf("%v", formating))
 	}
 }
