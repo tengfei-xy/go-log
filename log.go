41f42f222c02222a2a3cb7924fcbb1c27d88899e
tengfe-xy <taotengfei@zhiweidata.com>
2024-06-18 09:55:20 +0800

    添加设置日志等级的函数

--- log.go
+++ log.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"strings"
 	"time"
 )
 
@@ -22,152 +23,214 @@ func log(level string, formating string, args ...interface{}) {
 	fmt.Printf("%s %s %s:%d %s\n", level, time.Now().Format("2006-01-02 15:04:05"), filename, line, fmt.Sprintf(formating, args...))
 }
 
-const fatal = 0
+const LEVELFATAL string = "fatal"
+const LEVELERROR string = "error"
+const LEVELWARN string = "warn"
+const LEVELINFO string = "info"
+const LEVELDEBUG string = "debug"
+const LEVELDEBUG1 string = "debug1"
+const LEVELDEBUG2 string = "debug2"
+const LEVELDEBUG3 string = "debug3"
 
+const LEVELFATALINT int = 0
+const LEVELERRORINT int = 1
+const LEVELWARNINT int = 2
+const LEVELINFOINT int = 3
+const LEVELDEBUGINT int = 4
+const LEVELDEBUG1INT int = 5
+const LEVELDEBUG2INT int = 6
+const LEVELDEBUG3INT int = 7
+
+func SetLevelStr(v string) {
+	switch strings.ToLower(v) {
+	case LEVELFATAL:
+		SetLevelFatal()
+	case LEVELERROR:
+		SetLevelError()
+	case LEVELWARN:
+		SetLevelError()
+	case LEVELINFO:
+		SetLevelInfo()
+	case LEVELDEBUG:
+		SetLevelDebug()
+	case LEVELDEBUG1:
+		SetLevelDebug1()
+	case LEVELDEBUG2:
+		SetLevelDebug2()
+	case LEVELDEBUG3:
+		SetLevelDebug3()
+	default:
+		SetLevelInfo()
+	}
+}
+func SetLevelInt(v int) {
+	switch v {
+	case LEVELFATALINT:
+		SetLevelFatal()
+	case LEVELERRORINT:
+		SetLevelError()
+	case LEVELWARNINT:
+		SetLevelError()
+	case LEVELINFOINT:
+		SetLevelInfo()
+	case LEVELDEBUGINT:
+		SetLevelDebug()
+	case LEVELDEBUG1INT:
+		SetLevelDebug1()
+	case LEVELDEBUG2INT:
+		SetLevelDebug2()
+	case LEVELDEBUG3INT:
+		SetLevelDebug3()
+	default:
+		SetLevelInfo()
+	}
+}
 func SetLevelFatal() (int, string) {
-	level = 0
+	level = LEVELFATALINT
 	return level, "Fatal"
 
 }
 func SetLevelError() (int, string) {
-	level = 1
+	level = LEVELERRORINT
 	return level, "Error"
 
 }
 func SetLevelWarn() (int, string) {
-	level = 2
+	level = LEVELWARNINT
 	return level, "Warn"
 
 }
 func SetLevelInfo() (int, string) {
-	level = 3
+	level = LEVELINFOINT
 	return level, "Info"
 
 }
 func SetLevelDebug() (int, string) {
-	level = 4
+	level = LEVELDEBUGINT
 	return level, "Debug"
 
 }
 func SetLevelDebug1() (int, string) {
-	level = 5
+	level = LEVELDEBUG1INT
 	return level, "Debug1"
 
 }
 func SetLevelDebug2() (int, string) {
-	level = 6
+	level = LEVELDEBUG2INT
 	return level, "Debug2"
 
 }
 func SetLevelDebug3() (int, string) {
-	level = 7
+	level = LEVELDEBUG3INT
 	return level, "Debug3"
 }
 func GetLevel() (int, string) {
-	var str string = "Info"
+	var str string
 
 	switch level {
-	case 0:
-		str = "Fatal"
-	case 1:
-		str = "Error"
-	case 2:
-		str = "Warn"
-	case 3:
-		str = "Info"
-	case 4:
-		str = "Debug"
-	case 5:
-		str = "Debug1"
-	case 6:
-		str = "Debug2"
-	case 7:
-		str = "Debug3"
+	case LEVELFATALINT:
+		str = LEVELFATAL
+	case LEVELERRORINT:
+		str = LEVELERROR
+	case LEVELWARNINT:
+		str = LEVELWARN
+	case LEVELINFOINT:
+		str = LEVELINFO
+	case LEVELDEBUGINT:
+		str = LEVELDEBUG
+	case LEVELDEBUG1INT:
+		str = LEVELDEBUG1
+	case LEVELDEBUG2INT:
+		str = LEVELDEBUG2
+	case LEVELDEBUG3INT:
+		str = LEVELDEBUG3
+	default:
+		str = LEVELINFO
 	}
 	return level, str
 }
 func Fatalf(formating string, args ...interface{}) {
-	if level >= 0 {
+	if level >= LEVELFATALINT {
 		log("F", formating, args...)
 		os.Exit(125)
 	}
 }
 
 func Fatal(formating interface{}) {
-	if level >= 0 {
+	if level >= LEVELFATALINT {
 		log("F", fmt.Sprintf("%v", formating))
 		os.Exit(125)
 	}
 }
 
 func Errorf(formating string, args ...interface{}) {
-	if level >= 1 {
+	if level >= LEVELERRORINT {
 		log("E", formating, args...)
 	}
 }
 func Error(formating interface{}) {
-	if level >= 1 {
+	if level >= LEVELERRORINT {
 		log("E", fmt.Sprintf("%v", formating))
 	}
 }
 func Warnf(formating string, args ...interface{}) {
-	if level >= 2 {
+	if level >= LEVELWARNINT {
 		log("W", formating, args...)
 	}
 }
 func Warn(formating interface{}) {
-	if level >= 2 {
+	if level >= LEVELWARNINT {
 		log("W", fmt.Sprintf("%v", formating))
 	}
 }
 func Infof(formating string, args ...interface{}) {
-	if level >= 3 {
+	if level >= LEVELINFOINT {
 		log("I", formating, args...)
 	}
 }
 func Info(formating interface{}) {
-	if level >= 3 {
+	if level >= LEVELINFOINT {
 		log("I", fmt.Sprintf("%v", formating))
 	}
 }
 
 func Debugf(formating string, args ...interface{}) {
-	if level >= 4 {
+	if level >= LEVELDEBUGINT {
 		log("D", formating, args...)
 	}
 }
 func Debug(formating interface{}) {
-	if level >= 4 {
+	if level >= LEVELDEBUGINT {
 		log("D", fmt.Sprintf("%v", formating))
 	}
 }
 func Debug1f(formating string, args ...interface{}) {
-	if level >= 5 {
+	if level >= LEVELDEBUG1INT {
 		log("D", formating, args...)
 	}
 }
 func Debug1(formating interface{}) {
-	if level >= 5 {
+	if level >= LEVELDEBUG1INT {
 		log("D", fmt.Sprintf("%v", formating))
 	}
 }
 func Debug2f(formating string, args ...interface{}) {
-	if level >= 6 {
+	if level >= LEVELDEBUG2INT {
 		log("D", formating, args...)
 	}
 }
 func Debug2(formating interface{}) {
-	if level >= 6 {
+	if level >= LEVELDEBUG2INT {
 		log("D", fmt.Sprintf("%v", formating))
 	}
 }
 func Debug3f(formating string, args ...interface{}) {
-	if level >= 7 {
+	if level >= LEVELDEBUG3INT {
 		log("D", formating, args...)
 	}
 }
 func Debug3(formating interface{}) {
-	if level >= 7 {
+	if level >= LEVELDEBUG3INT {
 		log("D", fmt.Sprintf("%v", formating))
 	}
 }
